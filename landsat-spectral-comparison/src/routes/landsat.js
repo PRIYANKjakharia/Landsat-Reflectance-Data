b2882e07f47fce1727d8a606d7ea631ed1b742a9
Anik2812 <anikshaikh28125@gmail.com>
2024-10-06 23:48:52 +0530

    Revert "update"
    This reverts commit a4d83569ba4bd4bb48c1e297b738e83a80d468a1.

--- landsat-spectral-comparison/src/routes/landsat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const landsatController = require('../controllers/landsatController');
-
-router.get('/upcoming-passes', landsatController.getUpcomingPasses);
-router.get('/latest-scenes', landsatController.getLatestScenes);
-router.post('/update-notifications/:userId/:locationId', landsatController.updateNotifications);
-
-module.exports = router;
